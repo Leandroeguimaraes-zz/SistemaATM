5a16e7b1734455796e87a2de7dfe6f5642b20c96
Lucas Thimoteo <lucasthimoteo@id.uff.br>
2016-02-03 23:35:36 -0200

    criaçao das telas de boas vindas, criaçao de logo, criaçao da tela de login

---
+++ src/main.java
@@ -0,0 +1,20 @@
+
+import Views.BoasVindas;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Lucas Thimoteo
+ */
+public class main {
+    public static void main(String[] args) {
+        BoasVindas tela=new BoasVindas();
+        tela.setVisible(true);
+    }
+    
+}
